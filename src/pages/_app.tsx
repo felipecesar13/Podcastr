ba6789ab95407c63c57daede035d5f1bce10f5a1
felipe <felipecesar1304@gmail.com>
2021-09-03 12:05:51 -0300

    add code to git

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { PlayerContextProvider } from "../contexts/PlayerContext";
+
+import { Header } from "../components/Header";
+import { Player } from "../components/Player";
+
+import styles from "../styles/app.module.scss";
+import "../styles/global.scss";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <PlayerContextProvider>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
+  );
+};
+
+export default MyApp;
